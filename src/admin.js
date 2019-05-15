a9247db786c1f8c70cbeaf4a7f38bc6af3696628
RanranLiu <864749024@qq.com>
2019-05-15 15:12:35 +0800

    footer和content内容添加

--- src/admin.js
+++ src/admin.js
@@ -3,20 +3,21 @@ import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
+import Home from './pages/home'
 import './style/common.less'
 
 export default class Admin extends React.Component{
   render(){
     return (
       <Row className="container">
-        <Col span={3} className="nav">
+        <Col span={5} className="nav">
           <Nav/>
         </Col>
-        <Col span={21} className="main">
+        <Col span={19} className="main">
           <Header/>
           <Row className="content">
             {/* {this.props.children} */}
-            content
+            <Home/>
           </Row>
           <Footer/>
         </Col>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './index.less'
 
 export default class Footer extends React.Component{
   render(){
     return (
-      <div>
-        this is Footer
+      <div className="footer">
+        版权所有：（建议使用）
       </div>
     )
   }
